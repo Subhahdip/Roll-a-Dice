760b51cb705ff7f0a93bbb0204d0ba1224edd14a
Subhadip Sarkar <edusubhadip@gmail.com>
2023-09-11 20:48:32 +0530

    added the player switching condition in roll dice func

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let currentScore2 = 0;
 let score1 = 0;
 let score2 = 0;
 let diceValue = 0;
-
 let firstplayer = true;
 
 //starter
@@ -25,37 +24,66 @@ function newGame(params) {
   playerScoreEl1.textContent = 0;
   playerScoreEl2.textContent = 0;
   diceEl.classList.add('hidden');
+  section1.classList.remove('player--active');
+  section0.classList.add('player--active');
 }
 
 function rollDice(params) {
   diceValue = Math.floor(Math.random() * (6 - 1 + 1) + 1);
   diceEl.src = `dice-${diceValue}.png`;
   diceEl.classList.remove('hidden');
-
-  if (diceValue === 1) {
-    currentScore1 = 0;
-    currentScore2 = 0;
-    currentScoreEl1.textContent = currentScore1;
-    currentScoreEl2.textContent = currentScore2;
+  if (firstplayer) {
+    if (diceValue === 1) {
+      currentScore1 = 0;
+      currentScore2 = 0;
+      currentScoreEl1.textContent = currentScore1;
+      currentScoreEl2.textContent = currentScore2;
+      firstplayer = !firstplayer;
+      section0.classList.remove('player--active');
+      section1.classList.add('player--active');
+    } else {
+      currentScore1 += diceValue;
+      //currentScore2 = diceValue;
+      currentScoreEl1.textContent = currentScore1;
+      //currentScoreEl2.textContent = currentScore2;
+    }
   } else {
-    currentScore1 = diceValue;
-    currentScore2 = diceValue;
-    currentScoreEl1.textContent = currentScore1;
-    currentScoreEl2.textContent = currentScore2;
+    if (diceValue === 1) {
+      currentScore1 = 0;
+      currentScore2 = 0;
+      currentScoreEl1.textContent = currentScore1;
+      currentScoreEl2.textContent = currentScore2;
+      firstplayer = !firstplayer;
+      section1.classList.remove('player--active');
+      section0.classList.add('player--active');
+    } else {
+      //currentScore1 = diceValue;
+      currentScore2 += diceValue;
+      //currentScoreEl1.textContent = currentScore1;
+      currentScoreEl2.textContent = currentScore2;
+    }
   }
 }
 
 function hold(params) {
   if (firstplayer) {
-    score1 = currentScore1;
+    score1 += currentScore1;
     playerScoreEl1.textContent = score1;
     section0.classList.remove('player--active');
     section1.classList.add('player--active');
+    currentScore1 = 0;
+    currentScore2 = 0;
+    currentScoreEl1.textContent = currentScore1;
+    currentScoreEl2.textContent = currentScore2;
   } else {
-    score2 = currentScore2;
+    score2 += currentScore2;
     playerScoreEl2.textContent = score2;
     section1.classList.remove('player--active');
     section0.classList.add('player--active');
+    currentScore1 = 0;
+    currentScore2 = 0;
+    currentScoreEl1.textContent = currentScore1;
+    currentScoreEl2.textContent = currentScore2;
   }
   firstplayer = !firstplayer;
 }
